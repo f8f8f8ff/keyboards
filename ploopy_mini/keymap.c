248a81bd2b29dfc4cfe639c6e75c3fd7bbacbc4d
f8f8f8ff <reedhdonaldson@gmail.com>
2022-01-07 23:32:51 -0800

    add ploopy mini keymap

---
+++ ploopy_mini/keymap.c
@@ -0,0 +1,34 @@
+/* Copyright 2021 Colin Lam (Ploopy Corporation)
+ * Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ * Copyright 2019 Sunjun Kim
+ * Copyright 2019 Hiroyuki Okada
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum td_keycodes {
+    TD_BTN4_BTN5,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_BTN4_BTN5] = ACTION_TAP_DANCE_DOUBLE(KC_BTN4, KC_BTN5),
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT( /* Base */
+        KC_BTN1, KC_BTN3, KC_BTN2,
+        DRAG_SCROLL, TD(TD_BTN4_BTN5)
+    ),
+};
