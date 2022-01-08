56286577518c887016f2cefc996152d9ecff1dc1
reed <reedhdonaldson@gmail.com>
2022-01-07 22:49:27 -0800

    use shared leaders in lily58 keymap

--- lily58/keymap.c
+++ lily58/keymap.c
@@ -158,50 +158,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-LEADER_EXTERNS();
-void matrix_scan_user(void) {
-    LEADER_DICTIONARY() {
-        leading = false;
-        leader_end();
-
-        SEQ_TWO_KEYS(KC_W, KC_Q) {
-            SEND_STRING(SS_LALT(SS_TAP(X_F4)));
-        }
-        SEQ_TWO_KEYS(KC_S, KC_S) {
-            SEND_STRING(SS_TAP(X_PSCR));
-        }
-        SEQ_THREE_KEYS(KC_C, KC_A, KC_D) {
-            SEND_STRING(SS_LCTL(SS_LALT(SS_TAP(X_DEL))));
-        }
-        SEQ_ONE_KEY(KC_C) {
-            SEND_STRING(SS_TAP(X_CALC));
-        }
-        SEQ_ONE_KEY(KC_M) {
-            SEND_STRING(SS_TAP(X_MSEL));
-        }
-        SEQ_TWO_KEYS(KC_C, KC_S) {
-            SEND_STRING(SS_TAP(X_SYSTEM_SLEEP));
-        }
-#ifdef OLED_ENABLE
-/*
-        SEQ_TWO_KEYS(KC_O, KC_J) {
-            oled_set_brightness(1);
-        }
-        SEQ_TWO_KEYS(KC_O, KC_K) {
-            oled_set_brightness(255);
-        }
-        SEQ_TWO_KEYS(KC_O, KC_O) {
-            if (is_oled_on()) {
-                oled_off();
-            } else {
-                oled_on();
-            }
-        }
-*/
-#endif
-    }
-}
-
 void leader_start(void) {
     leader_listening = true;
 }
