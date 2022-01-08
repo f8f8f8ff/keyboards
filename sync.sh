bdbb42a29d1463d8cab87d8aff329c2b4f9b621c
reed <reedhdonaldson@gmail.com>
2022-01-07 22:16:57 -0800

    fix sync.sh again. correct destinations, and deletes files

--- sync.sh
+++ sync.sh
@@ -3,15 +3,24 @@
 set -e
 
 base_dir="$(cd "$(dirname "$0")" || exit 1; pwd)"
-keymap_name="f8"
 qmk_home="$HOME/qmk_firmware"
 
 printf 'using qmk firmware in %s.\n' "$qmk_home" 
 
+sync_files()
+{
+    src="$1"
+    dest="$2"
+    printf 'synchronizing %s\n' "$1"
+    rm -r "$dest"
+    cp -arT "$src" "$dest"
+}
+
 if [ -d "${qmk_home:?}" ]; then
     cd "${base_dir:?}" || exit 1
-    cp -ar "4x12" "${qmk_home:?}/keyboards/handwired/"
-    cp -ar "lily58" "${qmk_home:?}/keyboards/lily58/keymaps/${keymap_name:?}"
+    sync_files "userspace" "${qmk_home:?}/users/f8"
+    sync_files "4x12" "${qmk_home:?}/keyboards/handwired/4x12"
+    sync_files "lily58" "${qmk_home:?}/keyboards/lily58/keymaps/f8"
 fi
 
 printf 'done.\n'
