ddf6e75bd166110156a9f906e591c590d6df5ed7
f8f8f8ff <reedhdonaldson@gmail.com>
2023-12-07 15:56:36 -0800

    crkbd updates

--- crkbd/keymap.c
+++ crkbd/keymap.c
@@ -20,6 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum layer_number {
   _QWERTY = 0,
+  _COLEMAK,
+  _GAME,
   _LOWER,
   _RAISE,
   _ADJUST,
@@ -30,16 +32,28 @@ enum layer_number {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_split_3x6_3(
-	KY_TABG,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KY_TABG,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KY_ESCC,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LEAD,
+                          KC_LALT, MO(_LOWER), KC_SPC,                     KC_ENT, MO(_RAISE), KC_DEL
+),
+[_COLEMAK] = LAYOUT_split_3x6_3(
+    KY_TABG,  KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+    KY_ESCC,  KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                     KC_K,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,                     KC_M,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_LEAD,
+                          KC_LALT, MO(_LOWER), KC_SPC,                     KC_ENT, MO(_RAISE), KC_DEL
+),
+[_GAME] = LAYOUT_split_3x6_3(
+    KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LEAD,
                           KC_LALT, MO(_LOWER), KC_SPC,                     KC_ENT, MO(_RAISE), KC_DEL
 ),
 [_LOWER] = LAYOUT_split_3x6_3(
 	KC_DEL,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    LCTL_T(KC_BSPC),  KC_4,    KC_5,    KC_6,    KC_MINS, KC_PLUS,         KC_PIPE, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_GRV,
-    _______,  KC_7,   KC_8,    KC_9,    KC_0,    KC_DOT,                   KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_TILD,
-                            _______, _______, _______,                     _______, _______, _______
+    KC_ESC,   KC_6,    KC_7,    KC_8,    KC_9,      KC_0,                         KC_PIPE, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_GRV,
+    _______,  KC_BSPC,   KC_DQUO,    KC_MINS,    KC_SLSH,    KC_DOT,                   KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_TILD,
+                            KC_LGUI, _______, KC_ENT,                      _______, _______, _______
 ),
 [_RAISE] = LAYOUT_split_3x6_3(
 	_______,  KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
@@ -48,11 +62,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             _______, _______, _______,                     _______, _______, _______
 ),
 [_ADJUST] = LAYOUT_split_3x6_3(
-    RESET,    _______,  KC_MPLY, KC_VOLU,  KC_MSTP,  _______,              _______, _______, _______, KC_LPRN, KC_RPRN, _______,
+    QK_BOOT,    _______,  KC_MPLY, KC_VOLU,  KC_MSTP,  _______,              _______, _______, _______, KC_LPRN, KC_RPRN, _______,
     _______,  _______,  KC_MPRV, KC_VOLD,  KC_MNXT,  _______,              _______, _______, _______, KC_LCBR, KC_RCBR, _______,
     _______,  _______,  _______, KC_MUTE,  _______,  _______,              _______, _______, _______, KC_LBRC, KC_RBRC, _______,
-                                   _______, _______, _______,              _______, _______, _______
+                                   _______, _______, TG(_GAME),            TG(_COLEMAK), _______, _______
 ),
 };
 
-
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
